3a1be0e4812e864b07d5433fb1d15ec803d029c5
Nathan <nkeidel00@gmail.com>
2024-04-13 14:07:06 -0700

    added 'apt autoremove' and some other feautres to streamline process

--- tools/stress-test.sh
+++ tools/stress-test.sh
@@ -3,15 +3,15 @@
 # Licensing for this software can be found
 # In the working directory of this repository
 
-echo "Running a stress test using all system rescources..."
-
-sudo apt-get install stress-ng
+sudo apt-get install -y stress-ng
 
 max_cpu=$(nproc) # Maximum CPU cores
 max_io=$(grep -c processor /proc/cpuinfo) # Maximum IO operations (number of CPUs)
 max_vm=$(free -m | awk '/Mem:/{print $2}') # Maximum available memory in MB
 
 # Run stress-ng with the maximum values
-sudo stress-ng --cpu $max_cpu --io $max_io --vm $max_vm --vm-bytes 1G --timeout 600s
+clear; echo "Running a stress test using all system rescources..."
+sudo stress-ng --cpu $max_cpu --io $max_io --vm $max_vm --vm-bytes 1G --timeout 120s
 
-sudo apt-get purge stress-ng
+sudo apt-get purge -y stress-ng
+sudo apt autoremove -y
